12794776d9de69eaee765fd63cf297559fa4e05f
Georgia Channing <48959510+georgiachanning@users.noreply.github.com>
2022-09-27 19:01:48 +0300

    second commit

---
+++ generator.jl
@@ -0,0 +1,44 @@
+using MultiDocumenter
+
+clonedir = mktempdir()
+
+docs = [
+    ("TuringLang/DynamicPPL.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "ppl"),
+        path = "ppl",
+        name = "DynamicPPL"
+    ),
+    
+    ("TuringLang/AdvancedHMC.jl.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "hmc"),
+        path = "hmc",
+        name = "AdvancedHMC"
+    ),
+]
+
+#("TuringLang/turing.ml.git", "gh-pages") => MultiDocumenter.MultiDocRef(
+#        upstream = joinpath(clonedir, "tur"),
+#        path = "tur",
+#        name = "Turing"
+#    ),
+
+# using SSH for cloning is suggested when you're dealing with private repos, because
+# an ssh-agent will handle your keys for you
+# prefix = "git@github.com:"
+prefix = "https://github.com/"
+
+for ((remote, branch), docref) in docs
+    docref.upstream
+    run(`git clone --depth 1 $prefix$remote --branch $branch --single-branch $(docref.upstream)`)
+end
+
+outpath = joinpath(@__DIR__, "out")
+
+MultiDocumenter.make(
+    outpath,
+    collect(last.(docs));
+    search_engine = MultiDocumenter.SearchConfig(
+        index_versions = ["stable"],
+        engine = MultiDocumenter.FlexSearch
+    )
+)
